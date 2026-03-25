0506665406556517179bf9ecea6a2f5820681176
Jose Luis Delgado Quevedo <jose.delgado-q@uniminuto.edu.co>
2026-03-24 20:32:23 -0500

    HU-01 completado + UI actualización

--- bff/app/routers/zones.py
+++ bff/app/routers/zones.py
@@ -4,6 +4,4 @@ router = APIRouter()
 
 @router.get("/")
 async def get_zones():
-    # Aquí deberías llamar al microservicio de ingesta o transformación
-    # para obtener la lista de zonas. Por ahora, devolvemos un mock.
     return {"zones": ["Usaquén", "Chapinero", "Suba"]}
